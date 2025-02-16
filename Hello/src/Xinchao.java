6ff5d779e2f4fe02bd71e1ddf63c5464a3dc089a
NguyenQuocBao63 <bao.nq.63cntt@ntu.edu.vn>
2025-02-16 23:28:31 +0700

    câp nhật code
    sửa mới

--- Hello/src/Xinchao.java
+++ Hello/src/Xinchao.java
@@ -4,6 +4,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin Chao");
+		System.out.print("Nguyen Quoc Bao");
 
 	}
 
